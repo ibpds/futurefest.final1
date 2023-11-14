edd958524d5f1b931e648004efb89a25e1b9eb81
ibpds <145785503+ibpds@users.noreply.github.com>
2023-11-14 12:17:29 -0300

    Add files via upload

---
+++ futurefest/js/script.js
@@ -0,0 +1,30 @@
+function conteudo(){
+    window.location.href = `index10.html`
+}
+function pesquisa(){
+    window.location.href = `index8.html`
+}
+function vermais1(){
+    window.location.href = `index2.html`
+}
+function vermais2(){
+    window.location.href = `index3.html`
+}
+function vermais3(){
+    window.location.href = `index4.html`
+}
+function vermais4(){
+    window.location.href=`index5.html`
+}
+function vermais5(){
+    window.location.href=`index6.html`
+}
+function vermais6(){
+    window.location.href=`index7.html`
+}
+function inicio(){
+    window.location.href = `index1.html`
+}
+function telainicial(){
+    window.location.href = `index0.html`
+}
